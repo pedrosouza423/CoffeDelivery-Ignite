01c5a09a2a72dfa56c5558b03da85365dc73908f
Pedro Souza <62824231+pedrosouza423@users.noreply.github.com>
2023-01-08 21:40:18 -0300

    Application themes were created

---
+++ src/styles/default.ts
@@ -0,0 +1,18 @@
+export const defaultTheme = {
+  white: '#FFFFFF',
+  yellow: '#DBAC2C',
+  'yellow-dark': '#C47F17',
+  'yellow-light': '#F1E9C9',
+  puple: '#8047F8',
+  'purple-dark': '#4B2995',
+  'purple-light': '#EBE5F9',
+  'base-title': '#272221',
+  'base-subtitle': '#403937',
+  'base-text': '#574F4D',
+  'base-label': '#8D8686',
+  'base-hover': '#8D8686',
+  'base-button': '#8D8686',
+  'base-input': '#8D8686',
+  'base-card': '#8D8686',
+  background: '#FAFAFA',
+}
