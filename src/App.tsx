d535da8c3501211d06a46d2de221b07085c73e55
Pedro Souza <62824231+pedrosouza423@users.noreply.github.com>
2023-01-08 21:42:02 -0300

    The pages that will be built and the routes with the default layout were added

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { defaultTheme } from './styles/default'
+import { GlobalStyle } from './styles/global'
+
+export const App = () => {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
